ab442c77246f6c2f13eb08a678bb8e3bdb71bd17
Jaman <jaman.edux@gmail.com>
2021-08-12 19:59:26 +0200

    added routing

--- worldtime/lib/main.dart
+++ worldtime/lib/main.dart
@@ -1,8 +1,19 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:worldtime/pages/choose_location.dart';
+import 'package:worldtime/pages/loading.dart';
 import 'pages/home.dart';
+import 'pages/loading.dart';
+import 'pages/choose_location.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
 ));
 
 

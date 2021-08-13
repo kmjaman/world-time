57f345c2ec6d4810d3714e1200bae4e53f91d58c
Jaman <jaman.edux@gmail.com>
2021-08-14 00:40:43 +0200

    added locations

--- worldtime/lib/pages/choose_location.dart
+++ worldtime/lib/pages/choose_location.dart
@@ -1,4 +1,8 @@
+//import 'dart:html';
+//import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({ Key? key }) : super(key: key);
@@ -9,9 +13,22 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  @override
-  void initState() {
-    super.initState();
+  List<WorldTime> locations = [
+    WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin'),
+    WorldTime(location: 'London', flag: 'uk.png', url: 'Europe/London'),
+    WorldTime(location: 'New York', flag: 'usa.png', url: 'America/New_York'),
+  ];
+
+  void updateTime(index) async{
+    WorldTime instance = locations[index];
+    await instance.getTime();
+
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
   }
 
   @override
@@ -24,7 +41,25 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('choose location screen'),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+            child: Card(
+              child: ListTile(
+                title: Text(locations[index].location),
+                onTap: (){
+                  updateTime(index);
+                },
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              )
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- worldtime/lib/pages/home.dart
+++ worldtime/lib/pages/home.dart
@@ -16,7 +16,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-    data = ModalRoute.of(context)!.settings.arguments as Map;
+    data = data.isNotEmpty ? data : ModalRoute.of(context)!.settings.arguments as Map;
     print(data);
 
     String bgImage = data['isDayTime'] ? 'day.jpg' : 'night.jpg';
@@ -37,8 +37,16 @@ class _HomeState extends State<Home> {
             child: Column(
               children: [
                 TextButton.icon(
-                  onPressed: (){
-                    Navigator.pushNamed(context, '/location');
+                  onPressed: () async {
+                    dynamic result = await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'time': result['time'],
+                        'location': result['location'],
+                        'flag': result['flag'],
+                        'isDayTime': result['isDayTime'],
+                      };
+                    });
                   },
                   icon: Icon(
                     Icons.edit_location,
